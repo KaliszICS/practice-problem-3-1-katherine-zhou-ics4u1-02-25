4ef1da632839c446a736f2307da3bab7799b2570
katherine-zhou-ics4u1-02-25 <katherine.zhou3@student.tdsb.on.ca>
2025-04-15 01:34:08 +0000

    Update Practice

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -16,5 +16,18 @@ public class PracticeProblem {
 
 		return (x %10) + sumOfDigits(x/10);
 	}
-	}
 
+		public static int factorial(int n){
+			 
+			if (n == 0){
+				return 1;
+			}
+
+			if (n < 0){
+				return 0;
+			}
+
+			return n * factorial(n-1);
+
+		}
+	}
